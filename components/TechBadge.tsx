3db87ad07194746ca1f0b25dd0ce1c06a397b2e1
unknown <firas.jerbi.engineer@gmail.com>
2025-01-06 00:04:21 +0400

    label type

--- components/TechBadge.tsx
+++ components/TechBadge.tsx
@@ -1,8 +1,11 @@
-export default function TechBadge({ label })  : {label:any} {
-    return (
-      <span className="px-3 py-1 text-sm font-medium text-white bg-primary rounded-lg">
-        {label}
-      </span>
-    );
-  }
-  
+interface TechBadgeProps {
+  label: string; // Define the type of 'label' as string
+}
+
+export default function TechBadge({ label }: TechBadgeProps) {
+  return (
+    <span className="px-3 py-1 text-sm font-medium text-white bg-primary rounded-lg">
+      {label}
+    </span>
+  );
+}
