3f4395a558d77398697d57a0878a06ca97ba746f
unknown <firas.jerbi.engineer@gmail.com>
2025-01-06 00:02:16 +0400

    typo

--- components/TechBadge.tsx
+++ components/TechBadge.tsx
@@ -1,4 +1,4 @@
-export default function TechBadge({ label }) {
+export default function TechBadge({ label })  : {label:any} {
     return (
       <span className="px-3 py-1 text-sm font-medium text-white bg-primary rounded-lg">
         {label}
