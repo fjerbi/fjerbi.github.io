8a4fefa5c1450ca2c0f8b45c7c6f5a5d2ec5fd18
Firas Jerbi <firasjerbiv2@gmail.com>
2026-02-08 11:51:27 +0100

    google site verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,10 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <meta name="google-site-verification" content="NEDmAy1kKSil4bqazgW-WssV1GsyP6T_-TtbFejvHQs" />
+      <meta
+        name="google-site-verification"
+        content="bGvqA56lOU8jIWPDDN6sbtsU-rJ0Kelp-kyTl_YBKk4"
+      />
       <meta
         name="description"
         content="Firas Jerbi is a full-stack developer who specializes in building Web and Mobile projects and works on both frontend and backend."
