6ea48c6f95cefd31ac7ca12df448e159287054e2
Firas Jerbi <firasjerbiv2@gmail.com>
2026-04-13 12:38:42 +0200

    re-enable static export and add key prop

--- src/components/socials/SocialIcon.tsx
+++ src/components/socials/SocialIcon.tsx
@@ -1,11 +1,11 @@
 import { SocialLinkType } from '@/lib/types';
 
-import SocialLink from './SocialLink';
 import { Icon } from '@iconify/react';
+import SocialLink from './SocialLink';
 
 const SocialIcon = ({ icon, url }: SocialLinkType) => {
   return (
-    <li>
+    <li key={url}>
       <SocialLink href={url}>
         <Icon icon={icon} width={24} height={24} />
       </SocialLink>
